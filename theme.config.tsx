58232f194b94082f26d93b14a42a165087b0e7d7
Cruz <lunac8@unlv.nevada.edu>
2023-12-17 21:44:47 -0800

    setup, about, releases init

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,4 +5,13 @@ export default {
   },
   // ... other theme options
   docsRepositoryBase: "https://github.com/stratus-team/stratus-docs/blob/main",
+  // TODO: Update to dynamic
+  //https://nextra.site/docs/docs-theme/theme-configuration#dynamic-tags-based-on-page
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Stratus Docs" />
+      <meta property="og:description" content="Making dev's lives easier." />
+    </>
+  ),
 };
